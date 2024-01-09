b494eade7b142ea70c41f559bcfe9b6a49e662c8
Anuro <aldo_mms@hotmail.com>
2024-01-09 11:11:34 -0600

    modulo de peliculas

---
+++ peliculas/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+{
+    'name': 'Peliculas',
+    'summary': """
+        Módulo que implementa el catálogo de peliculas""",
+    'website': "https://www.gasomarshal.com",
+    'depends': [
+
+    ],
+    'author': 'Aldo Quintal',
+    "license": "LGPL-3",
+    'category': 'Contactos',
+    'description': '''
+    Agrega un nuevo campo en la sección de peliculas donde permite seleccionar de un catálogo peliculas
+    ''',
+    'data': [
+
+    ],
+    'images': ['static/description/icon.png'],
+    'application': True,
+    'installable': True,
+}
