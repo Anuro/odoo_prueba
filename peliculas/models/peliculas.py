537ab9f6c722acd581d92ea77de841f65b34502b
Anuro <aldo_mms@hotmail.com>
2024-01-09 12:10:57 -0600

    tab

--- peliculas/models/peliculas.py
+++ peliculas/models/peliculas.py
@@ -3,17 +3,17 @@ from odoo.osv import expression
 from odoo.exceptions import AccessError, UserError, ValidationError
 
 class PeliculasCine(models.Model):
-_name = "gsm.peliculas"
+    _name = "gsm.peliculas"
 
-name = fields.Char()
-number = fields.Integer()
-total = fields.Float()
-is_movie = fields.Boolean()
-category = fields.Selection([('terror1', 'Terror'),
-                            ('comedia2', 'Comedia')])
+    name = fields.Char()
+    number = fields.Integer()
+    total = fields.Float()
+    is_movie = fields.Boolean()
+    category = fields.Selection([('terror1', 'Terror'),
+                                ('comedia2', 'Comedia')])
 
-texto = fields.Html()
-fecha = fields.Datetime()
-imagen = fields.Binary()
-text = fields.Text()
+    texto = fields.Html()
+    fecha = fields.Datetime()
+    imagen = fields.Binary()
+    text = fields.Text()
 
